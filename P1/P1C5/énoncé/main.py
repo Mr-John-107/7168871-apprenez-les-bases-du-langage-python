62b7824fa0ec126ece29749e0ce15ade29d8c3b0
Ghost <jojo.retr0972@gmail.com>
2025-12-14 19:31:41 +0100

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,17 @@
-# Écrivez votre code ici !
+# Définition des variables
+nom = "Johnny"
+age = 25
+taille = 1.75
+est_etudiant = True
+
+# Affichage des valeurs des variables
+print(f"Nom: {nom}")
+print(f"Age: {age}")
+print(f"Taille: {taille}")
+print(f"Est étudiant: {est_etudiant}")
+
+# Affichage des types de chaque variable
+print(f"Type Nom: {type(nom)}")
+print(f"Type Age: {type(age)}")
+print(f"Type taille: {type(taille)}")
+print(f"Type Est étudiant: {type(est_etudiant)}")
