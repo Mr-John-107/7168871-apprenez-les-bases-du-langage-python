edbcaafe1c6ad35a702e797b88e1efd62c4e7c00
Ghost <jojo.retr0972@gmail.com>
2025-12-15 00:55:25 +0100

    Update main.py

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,22 @@
-# Écrivez votre code ici !
+# Création de la liste "fruits"
+fruits = ["pomme","banane","orange"]
+print(fruits)
+
+# Ajout de kiwi à la liste
+fruits.append("kiwi")
+print(fruits)
+
+# Suppression de "orange" de la liste
+fruits.remove("orange")
+print(fruits)
+
+# Modification du deuxième élément de la liste "fruits" en "ananas"
+fruits[1] = "ananas"
+print(fruits)
+
+# Affichage de la longueur
+len(fruits)
+
+# Tri de la liste "fruits" par ordre alphabétique
+fruits.sort()
+print(fruits)
